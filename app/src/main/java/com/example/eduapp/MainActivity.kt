4d999f0b8a9adc7e52a74518eb37762ffcd5f5c0
jc165984 <cue.nguyen@jcu.edu.au>
2026-03-26 17:42:07 +1000

    define sub-packages and move screens to the package screen.

--- app/src/main/java/com/example/eduapp/MainActivity.kt
+++ app/src/main/java/com/example/eduapp/MainActivity.kt
@@ -5,22 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.eduapp.screen.GameScreen
+import com.example.eduapp.screen.LandingScreen
+import com.example.eduapp.screen.ScoreScreen
+import com.example.eduapp.screen.SettingScreen
 import com.example.eduapp.ui.theme.EduAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,77 +45,4 @@ fun AppNav() {
 
     }
 
-}
-
-@Composable
-fun LandingScreen(navController: NavHostController, modifier: Modifier = Modifier) {
-    Scaffold(
-        topBar = { TopAppBar(title = { Text("Landing Screen") }) }
-    ) {
-            innerPadding ->
-            Column(modifier
-            .fillMaxSize()
-            .padding(innerPadding)
-            .padding(16.dp)) {
-            Button(onClick = {navController.navigate("setting")})
-            { Text("Go to Setting") }
-        }
-    }
-}
-
-@Composable
-fun SettingScreen(navController: NavHostController, modifier: Modifier = Modifier) {
-    Scaffold(
-        topBar = { TopAppBar(title = { Text("Setting Screen") }) }
-    ) {
-            innerPadding ->
-            Column(modifier
-            .fillMaxSize()
-            .padding(innerPadding)
-            .padding(16.dp)) {
-            Button(onClick = {navController.navigate("game")})
-            { Text("Play Game") }
-        }
-    }
-}
-
-@Composable
-fun GameScreen(navController: NavHostController, modifier: Modifier = Modifier) {
-    Scaffold(
-        topBar = { TopAppBar(title = { Text("Game Screen") }) }
-    ) {
-            innerPadding ->
-            Column(modifier
-            .fillMaxSize()
-            .padding(innerPadding)
-            .padding(16.dp)) {
-            Button(onClick = {navController.navigate("score")})
-            { Text("Go to Score") }
-        }
-    }
-}
-
-@Composable
-fun ScoreScreen(navController: NavHostController, modifier: Modifier = Modifier) {
-    Scaffold(
-        topBar = { TopAppBar(title = { Text("Score Screen") }) }
-    ) {
-            innerPadding ->
-        Column(modifier
-            .fillMaxSize()
-            .padding(innerPadding)
-            .padding(16.dp)) {
-            Button(onClick = {navController.navigate("landing")})
-            { Text("Go to Landing") }
-        }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EduAppTheme {
-        AppNav()
-    }
 }
